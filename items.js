696f23390685634b87a1258ee38e175416d379d0
Vince Aggrippino <vinceaggrippino@gmail.com>
2018-05-06 13:06:20 +0800

    implement getCategories()

--- items.js
+++ items.js
@@ -14,54 +14,49 @@
   limitations under the License.
 */
 
-
-const { MongoClient } = require('mongodb');
 const assert = require('assert');
 
-
-function ItemDAO(database) {
-  this.db = database;
+function ItemDAO(db) {
+  this.db = db;
 
   this.getCategories = (callback) => {
-    /*
-        * TODO-lab1A
-        *
-        * LAB #1A: Implement the getCategories() method.
-        *
-        * Write an aggregation query on the "item" collection to return the
-        * total number of items in each category. The documents in the array
-        * output by your aggregation should contain fields for "_id" and "num".
-        *
-        * HINT: Test your mongodb query in the shell first before implementing
-        * it in JavaScript.
-        *
-        * In addition to the categories created by your aggregation query,
-        * include a document for category "All" in the array of categories
-        * passed to the callback. The "All" category should contain the total
-        * number of items across all categories as its value for "num". The
-        * most efficient way to calculate this value is to iterate through
-        * the array of categories produced by your aggregation query, summing
-        * counts of items in each category.
-        *
-        * Ensure categories are organized in alphabetical order before passing
-        * to the callback.
-        *
-        */
-
+    const collection = db.collection('item');
     const categories = [];
-    const category = {
-      _id: 'All',
-      num: 9999,
-    };
 
-    categories.push(category);
+    collection.aggregate([
+      {
+        $group: {
+          _id: '$category',
+          num: { $sum: 1 },
+        },
+      },
+    ])
+      .toArray()
+      .then((result) => {
+        const total = result.reduce((accumulator, category) => {
+          categories.push(category);
+          return accumulator + category.num;
+        }, 0);
 
-    // TODO-lab1A Replace all code above (in this method).
+        categories.push({ _id: 'All', num: total });
 
-    // TODO Include the following line in the appropriate
-    // place within your code to pass the categories array to the
-    // callback.
-    callback(categories);
+        categories.sort((a, b) => {
+          /* eslint-disable no-underscore-dangle */
+          if (a._id < b._id) {
+            return -1;
+          } else if (a._id > b._id) {
+            return 1;
+          }
+          /* eslint-enable */
+          return 0;
+        });
+
+        callback(categories);
+      })
+      .catch((error) => {
+        console.error('An error occurred:');
+        console.error(error.message);
+      });
   };
 
 
@@ -154,7 +149,7 @@ function ItemDAO(database) {
 
     const item = this.createDummyItem();
     const items = [];
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 5; i += 1) {
       items.push(item);
     }
 
