2e83213b510f4a43c851882150465df614fb7014
Vince Aggrippino <vinceaggrippino@gmail.com>
2018-05-06 21:52:00 +0800

    Remove completed TODO comment

--- items.js
+++ items.js
@@ -85,30 +85,6 @@ function ItemDAO(db) {
   };
 
   this.searchItems = (query, page, itemsPerPage, callback) => {
-    /*
-         * TODO: lab2A
-         *
-         * LAB #2A: Implement searchItems()
-         *
-         * Using the value of the query parameter passed to searchItems(),
-         * perform a text search against the "item" collection.
-         *
-         * Sort the results in ascending order based on the _id field.
-         *
-         * Select only the items that should be displayed for a particular
-         * page. For example, on the first page, only the first itemsPerPage
-         * matching the query should be displayed.
-         *
-         * Use limit() and skip() and the method parameters: page and
-         * itemsPerPage to select the appropriate matching products. Pass these
-         * items to the callback function.
-         *
-         * searchItems() depends on a text index. Before implementing
-         * this method, create a SINGLE text index on title, slogan, and
-         * description. You should simply do this in the mongo shell.
-         *
-         */
-
     const collection = this.db.collection('item');
     const options = {
       limit: itemsPerPage,
